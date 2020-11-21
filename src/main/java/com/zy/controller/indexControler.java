425a5da8ab899d35e6394bff666f1d187ba9cbc8
zy <1198177057@qq.com>
2020-11-21 10:25:25 +0800

    init repo

---
+++ src/main/java/com/zy/controller/indexControler.java
@@ -0,0 +1,13 @@
+package com.zy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class indexControler {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
